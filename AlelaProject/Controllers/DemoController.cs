14be4f9ba3715db3c1eafa488a6048ec86bbad4d
APRENDIZ <APRENDIZ@BOGCPCDMF030306.FormacionSena.RED>
2024-03-09 11:00:54 -0500

    Version 1

---
+++ AlelaProject/Controllers/DemoController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AlelaProject.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
---
+++ AlelaProject/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using MOLA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MOLA.Models.Permiso;
+namespace MOLA.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+
+        public ActionResult Pagina()
+        {
+            return View();
+        }
+        [ValidarSesion]
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+
+            return View();
+        }
+
+        public ActionResult service()
+        {
+            return View();
+        }
+        public ActionResult Price()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult CerrarSesion()
+        {
+            Session["usuario"] = null;
+            return RedirectToAction("Login", "Acceso");
+        }
+
+        public ActionResult Turnos()
+        {
+            return RedirectToAction("Turnos", "Turnos");
+        }
+        public ActionResult Empleados()
+        {
+            return View();
+        }
+
+        
+
+
+
+
+    }
+}
